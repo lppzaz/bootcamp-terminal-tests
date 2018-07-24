128dcf14f159ef434a812fcaad11c2e2be27f5a0
lppzaz <cobus.pienaar@gmail.com>
2018-07-24 14:05:34 +0200

    fixing json problem with mocha

--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -10,12 +10,12 @@ describe("12. Checks all items quantities over 20",function(){
     {name : 'pears', qty : 37},
     {name : 'bananas', qty : 27},
     {name : 'apples', qty : 3}]),
-
-    [{name : 'pears', qty : 37},
+[{name : 'pears', qty : 37},
     {name : 'bananas', qty : 27}]);
   });
   it("checks what happens when all items under 20, returns empty array",function(){
-    assert.deepEqual(findItemsOver20(  [{name : 'apples', qty : 10},
+    assert.deepEqual(findItemsOver20( 
+  [{name : 'apples', qty : 10},
   {name : 'pears', qty : 7},
   {name : 'bananas', qty : 5},
   {name : 'apples', qty : 3}]),[]);
